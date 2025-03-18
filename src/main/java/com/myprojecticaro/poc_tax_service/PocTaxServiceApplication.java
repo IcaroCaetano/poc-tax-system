8eec4a3873bafa2b66cb7cc8f1a1daf976aa3982
IcaroCaetano <icaro.caetano@gmail.com>
2025-03-18 08:31:28 -0300

    add spring tool infrastructure

---
+++ src/main/java/com/myprojecticaro/poc_tax_service/PocTaxServiceApplication.java
@@ -0,0 +1,13 @@
+package com.myprojecticaro.poc_tax_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocTaxServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocTaxServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myprojecticaro/poc_tax_service/PocTaxServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myprojecticaro.poc_tax_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocTaxServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
